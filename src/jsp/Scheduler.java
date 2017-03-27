efaacbba51a346af1430ffa4f1133db817266c34
arntgm <agmalvik@gmail.com>
2017-03-27 13:09:37 +0200

    lj

--- src/jsp/Scheduler.java
+++ src/jsp/Scheduler.java
@@ -1,40 +1,27 @@
 package jsp;
 
+import jsp.ProblemCreator.Problem;
+
 public class Scheduler {
-	int[][] solution;
-	int[][] process;
-	int[][] machine;
-	int[] dueTime;
-	int jobs;
-	int machines;
-	int makespan;
-	int tardiness;
+
 	
-	public Scheduler(int[][] process, int[][] machine) {
-		this.process = process;
-		this.machine = machine;
-		this.jobs = process.length;
-		this.machines = process[0].length;
-		this.dueTime = new int[this.jobs];
-		for (int i = 0; i < jobs; i++) {
-			for (int j = 0; j < machines; j++) {
-				dueTime[i] += process[i][j];
-			}
-		}
-	}
 	
-	public void buildSchedule(int[] chromosome) {
-		solution = new int[machines][jobs];
+	public static int[] buildSchedule(int[] chromosome, Problem p) {
+		int[][] process = p.getProcMatrix();
+		int[][] machine = p.getMachMatrix();
+		int jobs = p.getNumJobs();
+		int machines = p.getNumMachines();
+		int[][] solution = new int[machines][jobs];
 		int[] nextTask = new int[jobs];
 		int[] jobStart = new int[jobs];
-		for (int i = 0; i < jobStart.length; i++) {
-			nextTask[i] = 1;
-		}
+//		for (int i = 0; i < jobStart.length; i++) {
+//			nextTask[i] = 1;
+//		}
 		int[] nextSol = new int[machines];
 		int[] machStart = new int[machines];
-		for (int i = 0; i < jobStart.length; i++) {
-			nextSol[i] = 1;
-		}
+//		for (int i = 0; i < jobStart.length; i++) {
+//			nextSol[i] = 1;
+//		}
 		for (int k = 0; k < chromosome.length; k++) {
 			int i = chromosome[k];
 			int j = machine[i][nextTask[i]];
@@ -45,24 +32,30 @@ public class Scheduler {
 			jobStart[i] = start + process[i][j];
 			machStart[j] = start + process[i][j];
 		}
-		makespanFitness(jobStart);
-		tardinessFitness(jobStart);
+		return jobStart;
 	}
 	
-	public void makespanFitness(int[] completionTimes) {
+	public static int makespanFitness(int[] completionTimes) {
 		int max = 0;
 		for (int i : completionTimes) {
 			if (i > max)
 				max = i;
 		}
-		makespan = max;
+		return max;
 	}
 	
-	public void tardinessFitness(int[] completionTimes) {
+	public static int tardinessFitness(int[] completionTimes, Problem p) {
 		int sum = 0;
-		for (int i = 0; i < jobs; i++) {
+		int[] dueTime = new int[p.getNumJobs()];
+		for (int i = 0; i < p.getNumJobs(); i++) {
+			for (int j = 0; j < p.getNumMachines(); j++) {
+				int[][] process = p.getProcMatrix();
+				dueTime[i] += process[i][j];
+			}
+		}
+		for (int i = 0; i < p.getNumJobs(); i++) {
 			sum += Math.max(completionTimes[i] - dueTime[i], 0);
 		}
-		tardiness = sum;
+		return sum;
 	}
 }
