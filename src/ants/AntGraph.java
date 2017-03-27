95956b1c5699df6d256f006db0f4b90d7a94d1ad
Bendik Witzoe <bendikw@gmail.com>
2017-03-27 11:49:07 +0200

    asd

--- src/ants/AntGraph.java
+++ src/ants/AntGraph.java
@@ -1,5 +1,19 @@
 package ants;
 
-public class AntGraph {
+import java.util.List;
 
+import jsp.ProblemCreator.Job;
+import jsp.ProblemCreator.Problem;
+
+public class AntGraph {
+	private double[][] pheromone;
+	private int[][] process;
+	private int jobs;
+	private int machines;
+	
+	
+	public AntGraph(Problem p){
+		this.jobs = p.getNumJobs();
+		this.machines = p.getNumMachines();
+	}
 }
