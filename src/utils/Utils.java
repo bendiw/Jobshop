ae666bff51487d38c4014c9531de0f13b33b994b
Bendik Witzoe <bendikw@gmail.com>
2017-05-02 15:22:32 +0200

    okok

--- src/utils/Utils.java
+++ src/utils/Utils.java
@@ -9,14 +9,12 @@ public class Utils {
 	public static int[] getJobArray(double[] position, int jobs){
 		int[] jobArray = new int[position.length];
 		double[] posCopy = position.clone();
-		int[] indexArray = new int[position.length];
+//		int[] indexArray = new int[position.length];
 		Arrays.sort(posCopy);
-//		System.out.println(Arrays.toString(this.position));
-//		System.out.println(Arrays.toString(posCopy));
 		for (int i = 0; i < jobArray.length; i++) {
 			int index = ArrayUtils.indexOf(posCopy,position[i]);
-			indexArray[i] = index;
-			jobArray[i] = index%(jobs);
+//			indexArray[i] = index;
+			jobArray[i] = ((index+1)%(jobs));
 		}
 //		System.out.println(Arrays.toString(indexArray));
 //		System.out.println(Arrays.toString(jobArray));
